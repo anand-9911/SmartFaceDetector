f7429ec3c948388beb0b0b71868f3167d210b324
Anand Gautam <Anand.Gautam@cognizant.com>
2020-02-20 21:48:22 +0530

    Commiting the changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,122 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import Rank from './components/Rank/Rank';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import FaceRecoginitation from './components/FaceRecoginitation/FaceRecoginitation';
+import SignIn from './components/SignIn/SignIn';
+import Register from './components/Register/Register';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+
+const app = new Clarifai.App({
+  apiKey: '8f936481d0af4cda975b0a9d4753b3a0'
+});
+
+
+const particlesOptions = {
+  "particles": {
+    "number": {
+      "value": 200
+    },
+    "size": {
+      "value": 3
+    }
+  },
+  "interactivity": {
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "repulse",
+        "color": "#00000"
+      }
+    }
+  }
 }
 
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {},
+      route: 'signin',
+      isSignedIn: 'false'
+    }
+  }
+  onInputChange = (event) => {
+    if (this.route === 'signout') {
+      this.setState({ isSignedIn: false })
+    } else if (this.route === 'home') {
+      this.setState({ isSignedIn: true })
+    }
+    this.setState({ input: event.target.value })
+  }
+
+  calculateFaceLocation = (data) => {
+    const calarifyFace = data.outputs[0].data.regions[0].region_info.bounding_box
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: calarifyFace.left_col * width,
+      topRow: calarifyFace.top_row * height,
+      rightCol: width - (calarifyFace.right_col * width),
+      bottomRow: height - (calarifyFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({ box: box })
+  }
+
+
+  onButtonSubmit = () => {
+    this.setState({ imageUrl: this.state.input });
+    app.models.predict(
+
+      Clarifai.FACE_DETECT_MODEL,
+      this.state.input)
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch(err => console.log(err))
+  }
+
+  onRouteChange = (route) => {
+    this.setState({ route: route });
+  }
+
+  render() {
+    return (
+      <div className="App">
+
+        <Particles className='particle'
+          params={particlesOptions}
+        />
+        <Navigation onRouteChange={this.onRouteChange} />
+        {
+          this.state.route === 'home'
+            ? <div>
+              <Logo />
+              <Rank />
+              <ImageLinkForm
+                onInputChange={this.onInputChange}
+                onButtonSubmit={this.onButtonSubmit}
+              />
+              <FaceRecoginitation box={this.state.box} imageUrl={this.state.imageUrl} />
+            </div>
+            : (
+              this.state.route === 'signin'
+                ? <SignIn onRouteChange={this.onRouteChange} />
+                : <Register onRouteChange={this.onRouteChange} />
+            )
+        }
+
+      </div>
+    )
+  }
+}
 export default App;
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Rank.css';
+
+const Rank = () => {
+    return (
+        <div>
+            <div className='black-90 f3'>
+                {'Anand,Your rank is...'}
+            </div>
+            <div className='black-90 f1'>
+                {'#5'}
+            </div>
+        </div>
+    );
+}
+
+export default Rank;
